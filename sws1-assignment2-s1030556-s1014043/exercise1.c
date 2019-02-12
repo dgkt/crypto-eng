d797c14bfed37346067c15672ae0baad9917a904
iwaps <iwap.saputra@yahoo.com>
2019-02-12 14:10:19 +0100

    1c

--- sws1-assignment2-s1030556-s1014043/exercise1.c
+++ sws1-assignment2-s1030556-s1014043/exercise1.c
@@ -1,6 +1,20 @@
+#include <stdio.h>
+
 int main (void){
-short i = 0x1234;
-char x = -127;
-long sn1 = <STUDENT NUMBER OF TEAM MEMBER 1, WITHOUT LEADING S>;
-long sn2 = <STUDENT NUMBER OF TEAM MEMBER 2, WITHOUT LEADING S>;
-int y[2] = {0x11223344,0x44332211};...}
+        short i = 0x1234;
+        char x = -127;
+        long sn1 = 1030556;
+        long sn2 = 1014043;
+        int y[2] = {0x11223344,0x44332211};
+
+	printf("Size of short: %ld bytes\n",sizeof(i));
+    	printf("Size of char: %ld bytes\n",sizeof(x));
+    	printf("Size of long sn1: %ld bytes\n",sizeof(sn1));
+    	printf("Size of long sn2: %ld byte\n",sizeof(sn2));
+	printf("Size of long int: %ld byte\n",sizeof(y));
+
+
+    return 0;
+
+}
+
