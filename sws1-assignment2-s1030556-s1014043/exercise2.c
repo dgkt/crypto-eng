bd80ad1e0f85797e5efe797eb1dcc66aa2d7cffc
rrl0203 <rrl0203@gmail.com>
2019-02-13 22:58:48 +0100

    Did 2d and corrected the makefile

--- sws1-assignment2-s1030556-s1014043/exercise2.c
+++ sws1-assignment2-s1030556-s1014043/exercise2.c
@@ -11,4 +11,16 @@ int main (){
 
 	test_bool = false;
 	printf ("False value is: 0x%x\n", test_bool);
+
+	char bigbool = 0x2;
+	bool b = (bool) bigbool;
+
+	if(b)
+	{
+		printf("%#02hhx as a boolean is true!\n", bigbool);
+	}	
+	else
+	{
+		printf("%#02hhx as a boolean is false!\n", bigbool);
+	}
 }
