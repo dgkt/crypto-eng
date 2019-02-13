b23ecd5c018d103a2ced8fa23704d5d846d49941
iwaps <iwap.saputra@yahoo.com>
2019-02-13 21:15:57 +0100

    no 2 revision

--- sws1-assignment2-s1030556-s1014043/exercise2.c
+++ sws1-assignment2-s1030556-s1014043/exercise2.c
@@ -1,18 +1,14 @@
 #include <stdbool.h>
 #include <stdio.h>
-#include <string.h>
 
-int main () {
-	int test_int = 0;
+int main (){
 	bool test_bool;
 
 	printf ("Size of bool is : %ld byte\n",sizeof(bool));
 	
 	test_bool = true;
-	memcpy (&test_int, &test_bool, sizeof(bool));
-	printf ("True value is: 0x%x\n", test_int);
+	printf ("True value is: 0x%x\n", test_bool);
 
 	test_bool = false;
-	memcpy (&test_int, &test_bool, sizeof(bool));
-	printf ("False value is: 0x%x\n", test_int);
+	printf ("False value is: 0x%x\n", test_bool);
 }
